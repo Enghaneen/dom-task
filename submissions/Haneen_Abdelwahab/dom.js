dd11b12ce75c159da62173f0a7f105e052863b85
Mohammed Abdel-Wahab <mohammednasser3042004@gmail.com>
2025-08-09 19:53:36 +0300

    Submission for DOM task -Haneen

---
+++ submissions/Haneen_Abdelwahab/dom.js
@@ -0,0 +1,59 @@
+//first task
+const lastElemntChild=document.body.lastElementChild.tagName;
+console.log(lastElemntChild)
+
+const lengthOfChildren=document.body.children.length;
+console.log(lengthOfChildren)
+
+const childrenLength=document.body.children.length
+const childrenNodesLength=document.body.childNodes.length
+console.log(childrenLength) //3 because it did not add the text nodes
+console.log(childrenNodesLength)//6 because it added the text nodes
+console.log("the difference is in the .children no text nodes is add to dom tree but in .childNode the text nodes is added ")
+
+const firstNode=document.body.childNodes[0]
+
+console.log(firstNode.nodeType)
+console.log(firstNode.nodeName)
+
+const firstParagraph=document.querySelector('p')
+const secondParagraph=document.querySelectorAll('p')[1]
+
+if(firstParagraph.parentElement === secondParagraph.parentElement){
+    console.log("Yes , both are siblings")
+} // why not use nextSibling? because it confirms they are siblings if they are only ! directly next to each other.
+
+
+const allNodes = document.body.childNodes;
+//console.log(allNodes) 
+allNodes.forEach((node, i) => {
+  if (node.nodeType === 3 ) {
+    console.log(`Text node at index ${i}:` , node);
+  } 
+})
+
+//second task
+const divEl=document.createElement('div')
+const h2El=document.createElement('h2')
+const pEl=document.createElement('p')
+
+divEl.className="card"
+divEl.dataset.role="admain"
+
+h2El.textContent="Access Panel"
+pEl.textContent="Authenticated"
+
+document.body.append(divEl)
+divEl.insertAdjacentElement("afterbegin",h2El)
+divEl.insertAdjacentElement("beforeend",pEl)
+
+console.log(divEl.dataset.role)
+pEl.textContent="Welcome back, Admin"
+
+divEl.classList.add("authenticated","highlight")
+
+console.log(divEl.classList) // card ,authenticated,highlight
+if(divEl.classList.contains("card")){
+    divEl.classList.remove("card")
+}
+console.log(divEl.classList) //authenticated,highlight
